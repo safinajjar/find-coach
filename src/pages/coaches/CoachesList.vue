f8f5c7b8f07bc023ef3521a7851138416f4d281d
Musti <safi.najjar1@gmail.com>
2024-12-31 21:21:52 +0100

    prepare CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,8 +8,23 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
   </section>
+  <ul v-if="hasCoaches">
+    <li v-for="coach in filteredCoaches" :key="coach">
+      {{ coach.firstName }}
+    </li>
+  </ul>
+  <h3 v-else>No Coaches found.</h3>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches']
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches']
+    },
+  },
+}
 </script>
