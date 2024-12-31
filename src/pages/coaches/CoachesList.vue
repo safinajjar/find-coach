ebc5405de394db3eb361d6ea1de420190222060e
Musti <safi.najjar1@gmail.com>
2024-12-31 22:00:37 +0100

    CoachesList and CoachItem

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,15 +9,38 @@
     </div>
   </section>
   <ul v-if="hasCoaches">
-    <li v-for="coach in filteredCoaches" :key="coach">
-      {{ coach.firstName }}
-    </li>
+    <coach-item
+      v-for="coach in filteredCoaches"
+      :key="coach.id"
+      :id="coach.id"
+      :first-name="coach.firstName"
+      :last-name="coach.lastName"
+      :rate="coach.hourlyRate"
+      :areas="coach.areas"
+    ></coach-item>
   </ul>
   <h3 v-else>No Coaches found.</h3>
 </template>
 
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
 <script>
+import CoachItem from '@/components/coaches/CoachItem.vue'
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches']
