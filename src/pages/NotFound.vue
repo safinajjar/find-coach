82d8ca3f6e8d18d3d082c524af50e3e3d13bf016
Musti <safi.najjar1@gmail.com>
2025-01-01 19:21:40 +0100

    add 404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,13 @@
-<template></template>
+<template>
+  <section>
+    <base-card>
+      <h1>Page not found</h1>
+      <p>
+        This page could not be found - check out our
+        <router-link :to="{ name: 'coachesList' }">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
 
 <script></script>
