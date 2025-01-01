a5babaaac02dea144c11b144033f6bf0e7343ecf
Musti <safi.najjar1@gmail.com>
2025-01-01 11:24:36 +0100

    register form

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,3 +1,18 @@
-<template>Register as Coach</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a Coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
 
-<script></script>
+<script>
+import CoachForm from '@/components/coaches/CoachForm.vue'
+
+export default {
+  components: {
+    CoachForm,
+  },
+}
+</script>
