224fb4310da6f795b034fba92ff91b7501845e60
Musti <safi.najjar1@gmail.com>
2025-01-01 13:13:42 +0100

    display incoming requests (mesages)

--- src/pages/requests/RequestsList.vue
+++ src/pages/requests/RequestsList.vue
@@ -1,3 +1,53 @@
-<template>Requests List</template>
+<template>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in requests"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
+</template>
 
-<script></script>
+<script>
+import RequestItem from '@/components/requests/RequestItem.vue'
+
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    requests() {
+      return this.$store.getters['requests/requests']
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests']
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
