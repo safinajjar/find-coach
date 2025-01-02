c66a7918025779f4d95f546e0b387ddc3595ee27
Musti <safi.najjar1@gmail.com>
2025-01-02 09:27:59 +0100

    added tryLogin

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,9 +2,29 @@ import axios from 'axios'
 
 export default {
   async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    })
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    })
+  },
+  async auth(context, payload) {
+    const mode = payload.mode
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAfBNsF_hJWe7Ns-momqiuwsjJHz7qnIeM'
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAfBNsF_hJWe7Ns-momqiuwsjJHz7qnIeM'
+    }
     try {
       const response = await axios({
-        url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAfBNsF_hJWe7Ns-momqiuwsjJHz7qnIeM`,
+        url,
         method: 'POST',
         data: {
           email: payload.email,
@@ -12,35 +32,29 @@ export default {
           returnSecureToken: true,
         },
       })
+
+      localStorage.setItem('token', response.data.idToken)
+      localStorage.setItem('userId', response.data.localId)
+
       context.commit('setUser', {
         token: response.data.idToken,
         userId: response.data.localId,
         tokenExpiration: response.data.expiresIn,
       })
     } catch (error) {
-      console.log(error)
       throw new Error(error.message || 'Failed to authenticate. Check your login data.')
     }
   },
-  async signup(context, payload) {
-    try {
-      const response = await axios({
-        url: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAfBNsF_hJWe7Ns-momqiuwsjJHz7qnIeM`,
-        method: 'POST',
-        data: {
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        },
-      })
+  tryLogin(context) {
+    const token = localStorage.getItem('token')
+    const userId = localStorage.getItem('userId')
+
+    if (token && userId) {
       context.commit('setUser', {
-        token: response.data.idToken,
-        userId: response.data.localId,
-        tokenExpiration: response.data.expiresIn,
+        token,
+        userId,
+        tokenExpiration: null,
       })
-    } catch (error) {
-      console.log(error)
-      throw new Error(error.message || 'Failed to authenticate. Check your login data.')
     }
   },
   logout(context) {
