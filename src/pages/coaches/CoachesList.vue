b8560b007b3df4ac7739e2ab4162d1b4ba373d99
Musti <safi.najjar1@gmail.com>
2025-01-02 08:34:10 +0100

    add isAuthenticated

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,7 +10,8 @@
       <base-card>
         <div class="controls">
           <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-          <base-button v-if="!isCoach && !isLoading" link to="/register"
+          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button v-if="isLoggedIn && !isCoach && !isLoading" link to="/register"
             >Register as Coach</base-button
           >
         </div>
@@ -54,6 +55,9 @@ export default {
     }
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated
+    },
     error() {
       return this.$store.getters['coaches/error']
     },
