4ce5200e8a50acec2b9e331378771a2ac3466636
Musti <safi.najjar1@gmail.com>
2025-01-02 16:11:19 +0100

    add async components for optimization and fix coachesList route

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CoachesList from './pages/coaches/CoachesList.vue'
-import CoachDetails from './pages/coaches/CoachDetails.vue'
-import ContactCoach from './pages/requests/ContactCoach.vue'
-import CoachRegister from './pages/coaches/CoachRegister.vue'
-import RequestsList from './pages/requests/RequestsList.vue'
-import NotFound from './pages/NotFound.vue'
-import UserAuth from './pages/auth/userAuth.vue'
 import store from './store'
+import { defineAsyncComponent } from 'vue'
+
+const CoachDetails = defineAsyncComponent(() => import('./pages/coaches/CoachDetails.vue'))
+const CoachRegister = defineAsyncComponent(() => import('./pages/coaches/CoachRegister.vue'))
+const ContactCoach = defineAsyncComponent(() => import('./pages/requests/ContactCoach.vue'))
+const RequestsList = defineAsyncComponent(() => import('./pages/requests/RequestsList.vue'))
+const NotFound = defineAsyncComponent(() => import('./pages/NotFound.vue'))
+const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'))
 
 const router = createRouter({
   history: createWebHistory(),
@@ -70,7 +72,7 @@ router.beforeEach((to, _, next) => {
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
     next('/auth')
   } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
-    next('/coachesList')
+    next({ name: 'coachesList' })
   } else {
     next()
   }
